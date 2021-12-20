786f354b6bc1c29e2e7c443d75d9bf7a6830c2db
Genesis <genesisybenitez1@gmail.com>
2021-12-19 20:09:53 -0500

    second commit

--- addEmployee.js
+++ addEmployee.js
@@ -1,48 +1,68 @@
 const timeNow = new Date(Date.now());
 
+
 $(document).ready(function(){
-$("#addEmployees").submit(function(event){
-event.preventDefault();
-var employeeName = document.getElementById("employeeName").value;
-var age = document.getElementById("age").value;
-let data = {
-"employeeName": employeeName,
-"age": age
-}
-console.log(data);
-fetch(("http://localhost:8080/post"),{
-method: 'POST', // *GET, POST, PUT, DELETE, etc.
-cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-credentials: 'same-origin', // include, *same-origin, omit
-headers: {
-'Content-Type': 'application/json'
-// 'Content-Type': 'application/x-www-form-urlencoded',
-},
-redirect: 'follow', // manual, *follow, error
-referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-body: JSON.stringify(data) // body data type must match "Content-Type" header
-}
+     $("#addEmployees").submit(function(event){
+        event.preventDefault();
+        var firstName = document.getElementById("firstName").value;
+        var lastName = document.getElementById("lastName").value;
+        var jobTitle = document.getElementById("jobTitle").value;
+        var age = document.getElementById("age").value;
+        let data = {
+            "firstName": firstName,
+            "lastName": lastName,
+            "jobTitle": jobTitle,
+            "age": age
+        }
+        console.log(data);
+        fetch(("http://localhost:8080/post"),{
+            method: 'POST', 
+            cache: 'no-cache', 
+            credentials: 'same-origin',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer', 
+            body: JSON.stringify(data) 
+          }
+        
+        ).then(response=> response)
+        .then(data=> {
+            console.log(data);
+            $("#container").append(`
+            <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+            <div class="toast" style="position: absolute; top: 0; right: 0;">
+                <div class="toast-header">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Echo_curation_alt_check_mark.svg/1024px-Echo_curation_alt_check_mark.svg.png" width="20" height="20" class="rounded me-2" alt="...">
+                <strong class="mr-auto text-success">Success</strong>
+                </div>
+                <div class="toast-body">
+                Employee successfully added at ${timeNow.toLocaleTimeString()}                
+                </div>
+            </div>
+            </div>
+            `)
+            $('.toast').toast('show');
+            // window.location = "http://127.0.0.1:5500/index.html";
 
-).then(response=> response)
-.then(data=> {
-console.log(data);
-$("#container").append(`
-<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
-<div class="toast" style="position: absolute; top: 0; right: 0;">
-<div class="toast-header">
-<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Echo_curation_alt_check_mark.svg/1024px-Echo_curation_alt_check_mark.svg.png" width="20" height="20" class="rounded me-2" alt="...">
-<strong class="mr-auto">Success</strong>
 
-</div>
-<div class="toast-body">
-Employee successfully added at ${timeNow.toLocaleTimeString()}
-</div>
-</div>
-</div>
-`)
-$('.toast').toast('show');
-
-})
-})
-
-})
+        }).catch(error=>{
+          $("body").append(`
+          <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+          <div class="toast" style="position: absolute; top: 0; right: 0;">
+              <div class="toast-header">
+              <img src="https://cdn0.iconfinder.com/data/icons/shift-interfaces/32/Error-512.png" width="20" height="20" class="rounded me-2" alt="...">
+              <strong class="mr-auto text-danger">Error</strong>
+              </div>
+              <div class="toast-body">
+               ${error} at ${timeNow.toLocaleTimeString()}                
+              </div>
+          </div>
+          </div>
+          `)
+          $('.toast').toast('show');
+        })
+     })
+     
+     })
--- script.js
+++ script.js
@@ -1,41 +1,53 @@
+const timeNow = new Date(Date.now());
+
+
+
 $(document).ready(function(){
-    fetch("http://localhost:8080/")
-    .then(response => response.json())
-    .then(data => {
-        
-        console.log(data)
-        for(let i=0; i < data.length; i++){
-
- 
-        $("#employees").append(
-            `
-            <div class = "col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 mt-4">
-            <div class="card" style="width: 14rem;">
-            <img src="https://cdn0.iconfinder.com/data/icons/dog-avatars/192/avatar-dog-doggy-saint-bernard-puppy-512.png" class="card-img-top"  width="100%" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Employee ID: ${data[i].id}</h5>
-                <p class="card-text">${data[i].employeeName}</p>
-                <a href="#" class="btn btn-danger" onclick="deleteEmployee(${data[i].id})">Delete Employee</a>
-            </div>
-            </div>
-            </div>
-            `
-        )
-    }
-    
-    });
-
-
-
+    fetch("http://localhost:8080")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        for(let i = 0; i < data.length; i++){
+        $("#employees").append(
+        `
+        <div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4 mt-4">
+            <div class="card text-center" style="width: 14rem;">
+                <img src="https://koolinus.files.wordpress.com/2019/03/avataaars-e28093-koolinus-1-12mar2019.png" class="card-img-top" width="100%" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${data[i].firstName + " " + data[i].lastName}</h5>
+                    <p class="card-text">${data[i].jobTitle}</p>
+                    <a href="#" class="btn btn-danger" onclick="deleteEmployee(${data[i].id})"><i class='fa fa-trash'></i> Delete</a>
+                </div>
+            </div>
+        </div>
+        `)
+                    
+        }
+    });
 })
 
 function deleteEmployee(id){
-    fetch((`http://localhost:8080/user/${id}`),{
-    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-    }).then(response => response)
-    .then(data => {
-        console.log(data)
-        window.location.reload();
-    })
-
+    console.log(`http://localhost:8080/deleteuser/${id}`)
+    fetch((`http://localhost:8080/deleteuser/${id}`),{
+            method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+        }).then(response=> response)
+        .then(data => {
+            console.log(data)
+            window.location.reload();
+            $("#container").append(`
+            <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+            <div class="toast" style="position: absolute; top: 0; right: 0;">
+                <div class="toast-header">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Echo_curation_alt_check_mark.svg/1024px-Echo_curation_alt_check_mark.svg.png" width="20" height="20" class="rounded me-2" alt="...">
+                <strong class="mr-auto">Success</strong>
+                </div>
+                <div class="toast-body">
+                Employee successfully deleted at ${timeNow.toLocaleTimeString()}                
+                </div>
+            </div>
+            </div>
+            `)
+            $('.toast').toast('show');
+        });
+        
 }
